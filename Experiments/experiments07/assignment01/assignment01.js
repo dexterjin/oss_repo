614c07bbed6abadc4bc4f3747655d1b35e07856e
Dexter Jin <dexter.jin@kiwiplus.io>
2019-04-17 20:53:08 +0900

    Experiments07 Materials Updated

--- Experiments/experiments07/assignment01/assignment01.js
+++ Experiments/experiments07/assignment01/assignment01.js
@@ -11,5 +11,8 @@ var bookList = new Array(new book("Perl1", "Mohtashim"),new book("Smith", "Mohta
 
 console.log(bookList);
 console.log("After Sorting");
-//bookList.sort(sortBook);
+bookList.sort(sortBook);
+// sortBook 이라는 comparing function을 완성하시오.
+// 1 순위 : title
+// 2 순위 : author
 console.log(bookList);
