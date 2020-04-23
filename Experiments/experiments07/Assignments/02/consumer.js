6d5e12d5c7d8d2cf8e1fc0b873a2f54f87b00d65
진성욱 <dexter.jin@kiwiplus.io>
2020-04-23 20:04:08 +0900

    Updated Experiments

--- Experiments/experiments07/Assignments/02/consumer.js
+++
@@ -1,9 +0,0 @@
-// Consumer 코드는 변경할 필요 없음
-var Producer = require('./producer');  
-var producer = new Producer();
-
-producer.on('add', function(t) {  
-	  console.log('add:', t);
-});
-
-producer.start();
